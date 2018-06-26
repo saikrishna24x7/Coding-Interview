4a8ef1bda6ec338710ea77219d60e8bec5f834cb
skuna <skuna@abb1055-027273.ad.lakeheadu.ca>
2018-06-26 12:15:52 -0400

    Commit 2

--- Coding-Interview/src/com/array/strings/StringPermutation.java
+++ Coding-Interview/src/com/array/strings/StringPermutation.java
@@ -1,9 +1,35 @@
 package com.array.strings;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StringPermutation
 {
+	
+	static Set<String> perms = new HashSet<String>();
 	public static void main(String[] args)
 	{
-		
+		perm1("abc");
+		perms.forEach(System.out::println);
 	}
+	
+	public  static void perm1(String s) { perm1("", s); }
+	
+	private static void perm1(String prefix, String s) 
+	{
+		//System.out.println("Prefix:"+prefix+", S:"+s);
+        int n = s.length();
+        if (n == 0) 
+        {
+        	//System.out.println(prefix);
+        	perms.add(prefix);
+        }
+        else 
+        {
+            for (int i = 0; i < n; i++) 
+            {
+            	perm1(prefix + s.charAt(i), s.substring(0, i) + s.substring(i+1, n));
+            }
+        }
+    }
 }
--- Coding-Interview/src/com/array/strings/StringUniqueChars.java
+++ Coding-Interview/src/com/array/strings/StringUniqueChars.java
@@ -2,7 +2,12 @@ package com.array.strings;
 
 public class StringUniqueChars
 {
-
+	
+	public static void checkUniqueChars(String input) 
+	{
+		
+	}
+	
 	public static void main(String[] args)
 	{
 		
